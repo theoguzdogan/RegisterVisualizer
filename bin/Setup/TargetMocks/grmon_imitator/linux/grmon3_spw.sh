d0465c9df14e208864574c3ba1c1070ef4988d5b
theoguzdogan <oguzdogan12@hotmail.com>
2023-09-21 15:01:37 +0300

    UPDATE: grmon imitator updated

--- bin/Setup/TargetMocks/grmon_imitator/linux/grmon3_spw.sh
+++ bin/Setup/TargetMocks/grmon_imitator/linux/grmon3_spw.sh
@@ -7,19 +7,43 @@ echo "$message"
 
 while true; do
 	echo -n "grmon3>"  # Print "Input: " without a newline character
-	# Read input from the user
-	read userInput
-	
-	if [ "$userInput" == "mem 0x040000AC 4" ]; then
-		echo " 0x040000AC	48700807		G.."
-	elif [ "$userInput" == "mem 0x06000500 4" ]; then
-		echo " 0x06000500	00000400		...."
-	elif [ "$userInput" == "mem 0x06000508 4" ]; then
-		echo " 0x06000508	01002000		.. ."
-	elif [ "$userInput" == "mem 0x80000024 4" ]; then
-		echo " 0x80000024	00000003		...."
-	else
-		echo "invalid entry"
-	fi
-	
+    # Read input from the user
+    read userInput
+    
+    if [[ $userInput == "mem"* ]]; then
+        # Split the input into space-separated parts
+        read -a inputParts <<< "$userInput"
+        
+        # Check if the input has at least three parts
+        if [ "${#inputParts[@]}" -ge 3 ]; then
+            keyword="${inputParts[0]}"
+            searchString="${inputParts[1]}"
+            
+            # Search for lines starting with the search string in the file
+            memory="../registers/scoc3.txt"
+            result=$(grep "^$searchString" "$memory")
+            
+            if [ -n "$result" ]; then
+                echo "$result"
+            fi
+        fi
+    elif [[ $userInput == "wmem"* ]]; then
+        # Split the "wmem" input into space-separated parts
+        read -a inputParts <<< "$userInput"
+        
+        # Check if the input has at least three parts
+        if [ "${#inputParts[@]}" -ge 3 ]; then
+            keyword="${inputParts[0]}"
+            address="${inputParts[1]}"
+            newString="${inputParts[2]}"
+            
+            # Update the file using awk to find and modify the line
+            memory="../registers/scoc3.txt"
+            awk -v address="$address" -v newString="$newString" '
+                BEGIN { FS = OFS = "\t" }
+                $1 == address { $2 = newString; }
+                { print; }
+            ' "$memory" > "$memory.tmp" && mv "$memory.tmp" "$memory"
+        fi
+    fi
 done
--- bin/Setup/TargetMocks/grmon_imitator/linux/test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-read userInput
-
-sleep 1
-
-echo "testtest"
